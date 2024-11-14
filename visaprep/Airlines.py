0577584d55b71c17addc1547ca435376ea6ba1cf
DayaBhavana <161931322+DayaBhavana@users.noreply.github.com>
2024-11-14 22:34:22 +0530

    Airlines.py

---
+++ visaprep/Airlines.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x,y=map(int,input().split())
+p=y-x*100
+x1=p/100
+x2=p//100
+if x1-x2>0:
+    print(x2+1)
+else:
+    print(x2)
