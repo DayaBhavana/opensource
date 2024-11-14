a8935b2a539dd074d6a3408ef890c4c6c8038e94
DayaBhavana <161931322+DayaBhavana@users.noreply.github.com>
2024-11-14 22:30:17 +0530

    RoundTable.py

---
+++ visaprep/RoundTable.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+x=int(input())
+if x<0:
+    print(0)
+else:
+    res1=(math.factorial(x))*(math.factorial(x+1))//(math.factorial(x+1))
+    print(res1)
