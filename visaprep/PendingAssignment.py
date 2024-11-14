423b34cdbe170f774b229edcd07b59dc92a7b19e
DayaBhavana <161931322+DayaBhavana@users.noreply.github.com>
2024-11-14 22:26:28 +0530

    PendingAssignment.py

---
+++ visaprep/PendingAssignment.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x,y,z=map(int,input().split())
+t_m=z*24*60
+r_m=x*y
+#res=t_m-r_m
+if(r_m<=t_m):
+    print("YES")
+else:
+    print("NO")
