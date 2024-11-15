96a4662eea525f9049465c3214917d8e2b0e5d04
DayaBhavana <161931322+DayaBhavana@users.noreply.github.com>
2024-11-15 12:00:41 +0530

    OnTimeArrival.py

---
+++ visaprep/OnTimeArrival.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+for i in range(n):
+    t=int(input())
+    if t>=30:
+        print("YES")
+    else:
+        print("NO")
