8d21972d4d52e3e9073e6d1c9f799a80964c4d47
DayaBhavana <161931322+DayaBhavana@users.noreply.github.com>
2024-11-18 19:13:44 +0530

    DND.py

---
+++ visaprep/DND.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+n1=[]
+n2=[]
+for i in a:
+    if i%m==0:
+        n1.append(i)
+    else:
+        n2.append(i)
+print(sum(n1)-sum(n2))
