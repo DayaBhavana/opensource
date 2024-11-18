7fa72315d86094e32dceb18d2d23cb03c6dceeea
DayaBhavana <161931322+DayaBhavana@users.noreply.github.com>
2024-11-18 19:21:02 +0530

    Pairsum.py

---
+++ visaprep/Pairsum.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+a=list(map(int,input().split()))
+t=int(input())
+c=0
+for i in range(n):
+    for j in range(i+1,n):
+        if i<j and a[i]+a[j]==t:
+            c+=1
+if c>0:
+    print("true")
+else:
+    print("false")
+            
